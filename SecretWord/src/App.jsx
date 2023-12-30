8100f723ef9c4ae19bb8518ce4d4cc73bdc6f8f2
RennoDani <daniela.g.renno@gmail.com>
2023-12-30 13:42:51 -0600

    call buttons/components

--- SecretWord/src/App.jsx
+++ SecretWord/src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import { wordsList } from "./data/words";
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  //console.log(words);
+
+  //start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //restart the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
-    <div>
-      Word Secret
-    </div>
-  )
+    <>
+      <div className='App'>
+        {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+        {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+        {gameStage === 'end' && <GameOver retry={retry}/>}
+      </div>
+    </>
+  );
 }
 
 export default App
---
+++ SecretWord/src/data/words.js
@@ -0,0 +1,5 @@
+export const wordsList = {
+    countries: ["Canada", "EUA", "Brazil"],
+    cities: ["Montreal", "Toronto", "Winnipeg", "Calgary", "Vancouver", "Sao Paulo", "Rio de Janeiro"],
+    animals: ["dog", "cat", "bear", "duck"],
+};
